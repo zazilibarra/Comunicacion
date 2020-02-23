101869889b7dbc5363b043f256de826a4a10520a
Zazil Ibarra <zaziil.97@gmail.com>
2020-02-22 22:43:14 -0600

    Comunicacion localhost

---
+++ src/comunicacion/Cliente.java
@@ -0,0 +1,45 @@
+/**
+ * @author Alvarez Esaú
+ * @author Ibarra Zazil
+ * @author Torres Daniel
+ */
+
+package comunicacion;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+import java.util.logging.*;
+
+public class Cliente extends Thread {
+    protected Socket sk;
+    protected DataOutputStream dos;
+    protected DataInputStream dis;
+    private String id;
+    
+    public Cliente(String name) {
+        id = name;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            sk = new Socket("127.0.0.1", 10578);
+            dos = new DataOutputStream(sk.getOutputStream());
+            dis = new DataInputStream(sk.getInputStream());
+            System.out.println(id + " envía saludo");
+            dos.writeUTF("hola");
+            String respuesta="";
+        } 
+        catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Thread client = new Cliente("Cliente");
+        client.start();
+    }
+}
---
+++ src/comunicacion/ServidorHilo.java
@@ -0,0 +1,83 @@
+/**
+ * @author Alvarez Esaú
+ * @author Ibarra Zazil
+ * @author Torres Daniel
+ */
+
+package comunicacion;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.*;
+
+public class ServidorHilo extends Thread {
+    private Socket socket;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+    private String idCliente;
+    private String estado;
+    
+    public ServidorHilo(Socket socket, int id) {
+        this.socket = socket;
+        this.idCliente = "Sensor" + id;
+        this.estado = "ACTIVO";
+        try {
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+        } 
+        catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /*Recibe mensaje del cliente*/
+    @Override
+    public void run() {
+        String accion = "";
+        
+        try {
+            accion = dis.readUTF();
+            if(accion.equals("hola")){
+                System.out.println("El Sensor con id "+this.idCliente+" saluda");
+            }
+        } 
+        catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+     public String getIP()
+    {
+      String ip;
+      ip=new String(this.socket.getInetAddress()+"");
+      return ip;
+    }
+    
+    public void setEstado(String nEstado)
+    {
+      this.estado=nEstado;
+    }
+    
+    public String getEstado()
+    {
+      return this.estado;
+    }
+    
+    public void setidCliente(String nNombre)
+    {
+      this.idCliente=nNombre;
+    }
+    
+    public String getidCliente()
+    {
+      return idCliente;
+    }
+    
+    public void desconnectar() {
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorHilo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
