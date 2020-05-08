b5539f6a8a6dc01a31f1dcf5b2f24bebcddf0e59
= <=>
2020-05-08 14:20:26 -0500

    Estructura del mensaje

---
+++ src/comunicacion/Helper.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comunicacion;
+
+import java.nio.ByteBuffer;
+
+/**
+ *
+ * @author USUARIO DELL
+ */
+public class Helper {
+    
+    public static byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
+    
+    public static long bytesToLong(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.put(bytes);
+        buffer.flip();//need flip 
+        return buffer.getLong();
+    }
+}
