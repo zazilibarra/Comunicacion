d16b31aac2fa3cedf0f99255a08127134a1c2f0d
Zazil Ibarra <zaziil.97@gmail.com>
2020-05-22 12:00:38 -0500

    Proyecto Servidor

--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sevidor;
+package servidor;
 
 /**
  *
  * @author DELL
  */
-public class Sevidor {
+public class Servidor {
 
     /**
      * @param args the command line arguments
